94c0d96058f8182b2d7474a53d6a4da1b5df8400
Diego Calvo <diegocc6@gmail.com>
2021-05-04 13:23:10 +0200

    disable sms verification

---
+++ app/models/custom/concerns/verification.rb
@@ -0,0 +1,14 @@
+require_dependency Rails.root.join('app', 'models', 'concerns', 'verification').to_s
+
+module Verification
+  extend ActiveSupport::Concern
+
+  def sms_verified?
+    true
+  end
+
+  def verification_letter_sent?
+    true
+  end
+
+end
