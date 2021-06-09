50d1bc4518c3ceb890e4d9aca496e86dba650a60
Diego Calvo <diegocc6@gmail.com>
2021-06-09 08:44:25 +0200

    sentry

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,5 +1,5 @@
 Sentry.init do |config|
-  config.dsn = 'https://671260854a5c43d2b987af9d11d38db6@sentry.enreda.coop/2'
+  config.dsn = Rails.application.secrets.sentry_dsn
   config.breadcrumbs_logger = [:active_support_logger, :http_logger]
 
   # To activate performance monitoring, set one of these options.
